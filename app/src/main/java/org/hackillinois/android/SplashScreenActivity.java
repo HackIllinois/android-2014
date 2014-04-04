7f63f8b0128ff3c18e7de5602a2459fdf8394aac
Nelson Osacky <nelson@osacky.com>
2014-04-04 16:06:27 -0500

    faster splash

--- app/src/main/java/org/hackillinois/android/SplashScreenActivity.java
+++ app/src/main/java/org/hackillinois/android/SplashScreenActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.VideoView;
@@ -26,12 +27,12 @@ public class SplashScreenActivity extends Activity {
         int uiOptions = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
         decorView.setSystemUiVisibility(uiOptions);
-
-
+        final ImageView splash = (ImageView) findViewById(R.id.splash_image);
         final ImageView button = (ImageView) findViewById(R.id.launchbutton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                v.setVisibility(View.INVISIBLE);
                 Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
                 startActivity(intent);//GoogleAuthActivity.class));
                 overridePendingTransition(R.anim.slide_in_up, R.anim.slide_out_up);
@@ -43,8 +44,15 @@ public class SplashScreenActivity extends Activity {
         video.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mp) {
-                //video.requestFocus();
                 video.start();
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        splash.setVisibility(View.INVISIBLE);
+                    }
+                }, 100);
+
             }
         });
         video.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
