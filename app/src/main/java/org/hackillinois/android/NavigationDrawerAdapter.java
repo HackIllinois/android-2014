42b70d06233eda32c8d6e0054b3d889ace1fb8ff
Vishal Disawar <disawar2@illinois.edu>
2014-04-10 18:50:12 -0500

    nav drawer highlighting works on high mem devices

--- app/src/main/java/org/hackillinois/android/NavigationDrawerAdapter.java
+++ app/src/main/java/org/hackillinois/android/NavigationDrawerAdapter.java
@@ -1,6 +1,7 @@
 package org.hackillinois.android;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,7 +16,7 @@ public class NavigationDrawerAdapter extends ArrayAdapter<String> {
     private final Context context;
     private final String[] values;
     //int[] deselectedIcons = {R.drawable.ic_person_deselected, R.drawable.ic_people_tab_deselected, R.drawable.ic_feed_tab_deselected, R.drawable.ic_schedule_tab_deselected, R.drawable.ic_support_tab_deselected};
-    //int[] deselectedIcons = {R.drawable.ic_person_deselected, R.drawable.ic_people_tab_deselected, R.drawable.ic_feed_tab_deselected, R.drawable.ic_schedule_tab_deselected, R.drawable.ic_support_tab_deselected};
+    int[] selectedIcons = {R.drawable.ic_person_selected, R.drawable.ic_people_tab_selected, R.drawable.ic_feed_tab_selected, R.drawable.ic_schedule_tab_selected, R.drawable.ic_support_tab_selected};
     int[] icons;
 
     public NavigationDrawerAdapter(Context context, String[] values, int[] icons) {
@@ -28,6 +29,7 @@ public class NavigationDrawerAdapter extends ArrayAdapter<String> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder viewHolder;
+        boolean wasNull = false;
 
         if (convertView == null) {
             LayoutInflater inflater = (LayoutInflater) context
@@ -40,17 +42,24 @@ public class NavigationDrawerAdapter extends ArrayAdapter<String> {
             viewHolder.iconImageView = (ImageView) convertView.findViewById(R.id.icon_drawer);
 
             convertView.setTag(viewHolder);
+            wasNull = true;
         } else {
             viewHolder = (ViewHolder) convertView.getTag();
+            wasNull = false;
         }
 
-//        for(int i = 0; i < deselectedIcons.length; i++){
-//            viewHolder.iconImageView.setImageResource(deselectedIcons[i]);
-//        }
+        if(wasNull){
+            viewHolder.textView.setText(values[position]);
+            if(position == 0){
+                viewHolder.textView.setTextColor(Color.WHITE);
+                viewHolder.iconImageView.setImageResource(icons[position]);
+                viewHolder.iconImageView.setImageResource(selectedIcons[0]);
+            } else
+            viewHolder.iconImageView.setImageResource(icons[position]);
 
-        viewHolder.textView.setText(values[position]);
-        viewHolder.iconImageView.setImageResource(icons[position]);
-        viewHolder.iconImageView.invalidate();
+        }
+        //viewHolder.iconImageView.setImageResource(icons[position]);
+        //viewHolder.iconImageView.invalidate();
         return convertView;
     }
 
