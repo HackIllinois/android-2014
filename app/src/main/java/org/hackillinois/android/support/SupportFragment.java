2ed09485b639563120c156bd5ae2a7d6f3964e96
Ben Fishbein <fishbeinb@gmail.com>
2014-04-11 05:50:55 -0500

    got rid of useless logs

--- app/src/main/java/org/hackillinois/android/support/SupportFragment.java
+++ app/src/main/java/org/hackillinois/android/support/SupportFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
+import android.support.v7.app.ActionBar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
